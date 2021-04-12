14913c1bee5ea31e94922140fcbc39520a5f3842
alexander <alexander_5560@hotmail.com>
2021-04-12 14:34:19 +0200

    Autohuur done

--- Autoverhuur/src/AutoHuur.java
+++ Autoverhuur/src/AutoHuur.java
@@ -1,2 +1,37 @@
 public class AutoHuur {
+    int aantalDagen;
+    Auto gehuurdeAuto;
+    Klant huurder;
+
+    public AutoHuur() {
+    }
+
+    public int getAantalDagen() {
+        return aantalDagen;
+    }
+
+    public Auto getGehuurdeAuto(){
+        return gehuurdeAuto;
+    }
+
+    public Klant getHuurder(){
+        return huurder;
+    }
+
+    public void setAantalDagen(int aantalDagen) {
+        this.aantalDagen = aantalDagen;
+    }
+
+    public void setGehuurdeAuto(Auto gehuurdeAuto){
+        this.gehuurdeAuto = gehuurdeAuto;
+    }
+
+    public void setHuurder(Klant huurder){
+        this.huurder = huurder;
+    }
+
+    public double totaalPrijs(){
+        return gehuurdeAuto.getPrijsPerDag() * aantalDagen;
+    }
+
 }
