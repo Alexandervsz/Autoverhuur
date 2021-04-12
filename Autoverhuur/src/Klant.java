3d5042bcced534e740a7539b24144c0354ad9c5d
alexander <alexander_5560@hotmail.com>
2021-04-12 14:49:17 +0200

    Klant done.

--- Autoverhuur/src/Klant.java
+++ Autoverhuur/src/Klant.java
@@ -1,2 +1,21 @@
 public class Klant {
+    String naam;
+    Double kortingsPercentage;
+
+    public Klant(String naam) {
+        this.naam = naam;
+    }
+
+    public Double getKortingsPercentage() {
+        return kortingsPercentage;
+    }
+
+    public void setKortingsPercentage(Double kortingsPercentage) {
+        this.kortingsPercentage = kortingsPercentage;
+    }
+
+    @Override
+    public String toString() {
+        return naam+" (korting: "+kortingsPercentage+"%)";
+    }
 }
