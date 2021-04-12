e0b5037be5a5e9579c7e3e25bdced71a4029b0fe
alexander <alexander_5560@hotmail.com>
2021-04-12 15:52:44 +0200

    Finished game.

--- Gameswinkel/src/Game.java
+++ Gameswinkel/src/Game.java
@@ -1,2 +1,19 @@
+import java.time.LocalDate;
+import java.util.Date;
+
 public class Game {
+    String name;
+    int releaseYear;
+    Double sellingPrice;
+
+    public Game(String name, int releaseYear, Double sellingPrice) {
+        this.name = name;
+        this.releaseYear = releaseYear;
+        this.sellingPrice = sellingPrice;
+    }
+
+    public double getPrice() {
+        int currentYear = LocalDate.now().getYear();
+        return sellingPrice * Math.pow(0.7, (currentYear - releaseYear));
+    }
 }
